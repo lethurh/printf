52408b2d7f6b93bf7273f700d362557c1d3b7bab
lethurh <gugulethupraxedis@gmail.com>
2024-06-03 10:41:20 -0700

    Write a function that produces output according to a format.

---
+++ _printf.c
@@ -0,0 +1,82 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ *checker - checkes whether next format is identifier
+ *@arr:array of aidentifier
+ *@format:next format to%
+ *Return:int
+ */
+int checker(char *arr, char format)
+{
+	int count = 0;
+	int k = 0;
+	int get = 1;
+
+	while (arr[k] != '\0')
+	{
+		count++;
+		k++;
+	}
+	for (k = 0; k < count; k++)
+	{
+		if (arr[k] == format)
+		{
+			get = 0;
+			break;
+		}
+	}
+	return (get);
+
+}
+/**
+ * _printf - prints to output according to format
+ * @format: character string
+ *
+ * Return:numbers of characters
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0, j = 0;
+
+	int (*f)(va_list);
+	va_list list;
+	int get;
+	char *arr = "cdisbuoxX%";
+
+	va_start(list, format);
+
+	if (format == NULL || !format[i + 1])
+		return (-1);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1])
+			{
+				get = checker(arr, format[i + 1]);
+				if (get == 1)
+				{
+					j += putchar(format[i]);
+					j += putchar(format[i + 1]);
+					i++;
+				}
+				else
+				{
+					f = caller(&format[i + 1]);
+					j += f(list);
+					i++;
+				}
+			}
+		}
+		else
+		{
+			j += putchar(format[i]);
+		}
+		i++;
+	}
+	va_end(list);
+	return (j);
+}
---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * struct func_type - type structure
+ * @t: pointer to the argument
+ * @f: pointer-function associated with the argument
+ */
+typedef struct func_type
+{
+	char *t;
+	int (*f)(va_list);
+} func_t;
+
+int (*caller(const char *format))(va_list);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_str(va_list args);
+int print_char(va_list args);
+int print_pct(va_list args);
+int print_dec(va_list args);
+int print_bin(va_list args);
+int print_un(va_list args);
+int print_oct(va_list args);
+int print_hex(va_list args);
+int print_HEX(va_list args);
+
+#endif
